07b612fc5c6e776285d4b1ef7c278659e8f94ee2
zenos11-alt <114966762+zenos11-alt@users.noreply.github.com>
2026-04-07 09:46:02 -0300

    final projetc 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,20 +1,52 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+import { Pet } from '../models/pet';
 
-const home = (req: Request, res: Response) => {
-    // res.render('pages/page');
-    res.send('Home');
+export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();//pega todos os pets
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list //lista de pets
+    });
 };
 
- const dogs = (req: Request, res: Response) => {
-    // res.render('pages/page');
+export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');//pega os pets do tipo dog
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list //lista de cachorros
+    });
 };
 
-const cats = (req: Request, res: Response) => {
-    // res.render('pages/page');
+export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');//pega os pets do tipo cat
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list //lista de gatos
+    });
 };
 
-const fishes = (req: Request, res: Response) => {
-    // res.render('pages/page');
+export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');//pega os pets do tipo fish
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list //lista de peixes
+    });
 };
-
-export { home, dogs, cats, fishes };
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,22 @@
 import type { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
-  res.send("ok");
+    let query: string = req.query.q as string;
+
+    if (!query) {
+        res.redirect('/');//se não tiver query, redireciona para a home
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query //passa a query para a página, para que ela possa mostrar o que foi pesquisado
+    //value fica no input- header-mustache
+  })
 };
 
